f950776af9ecfabbefac356b8fc98058d12c8c2e
Satya <satyabhr2005@gmail.com>
2026-09-10 23:02:20 +0530

    Implemented Module-4: Dataset Ingestion and Inspection

--- backend/src/authentication/validators/authValidator.js
+++
@@ -1,4 +0,0 @@
-// authValidator.js
-// Basic placeholder for more advanced schema validation (e.g., Joi, express-validator)
-// The current implementation in authController.js handles basic validation.
-module.exports = {};
